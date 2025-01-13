d556275e5dd8563120729d7eff2d9483a8867be5
keirbreakwell <keirbreakwell@hotmail.co.uk>
2025-01-13 20:49:21 +0000

    Revert next.config.js to stable version

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -5,10 +5,6 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-  experimental: {
-    optimizeCss: true,    // CSS optimization
-    forceSwcTransforms: true,  // Force faster transforms
-  },
 };
 
 module.exports = nextConfig;
